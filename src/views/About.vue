a7b34744badd77db5aee518bffbce98d86794c3a
mario <panggihmario@gmail.com>
2018-07-27 06:25:17 +0700

    add layout room

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,65 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <a class="waves-effect waves-light btn">button</a>
-<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
-<a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a>
+    <div>
+        <navbarR/>
         
-  </div>
+     <div class="row">
+      <div class="col s3 ">
+        <!-- Grey navigation panel -->
+        <div class="card small white">
+        <div class="card-content grey-text text-darken-4">
+        <span class="card-title  grey lighten-1 ">User A</span>
+        <div class="col s12">
+        <ul>
+          <li class="collection text-darken-2">name :</li>
+          <li class="collection">score :</li>
+          <li class="collection pink-text"><i class="fas fa-heart"></i> :</li>
+      </ul>
+      </div>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+
+        <!-- Grey navigation panel -->
+         <div class="card small white">
+        <div class="card-content grey-text text-darken-4">
+        <span class="card-title  grey lighten-1 ">User A</span>
+        <div class="col s12">
+        <ul>
+          <li class="collection text-darken-2">name :</li>
+          <li class="collection">score :</li>
+          <li class="collection pink-text"><i class="fas fa-heart"></i> :</li>
+      </ul>
+      </div>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+      </div>
+      <div class="col s9 black" >
+        <!-- quetion here  -->c
+      </div>
+    </div>
+
+    <footerR/>
+
+
+   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import navbarR from '@/components/navbarRoom.vue'
+import footerR from '@/components/footerRoom.vue'
+
+export default {
+  name: 'room',
+  components: {
+    navbarR,footerR
+  }
+}
+</script>
