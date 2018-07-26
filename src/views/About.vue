a31476e1bd70d58e3ffac553c3ab52206b7a76a1
“Ollas2204” <fitrisakinah17@gmail.com>
2018-07-26 20:04:03 +0700

    add cdn fire & materialize

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a class="waves-effect waves-light btn">button</a>
+<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+<a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a>
+        
   </div>
 </template>
