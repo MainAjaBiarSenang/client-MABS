e015f45964fcf1f02abcdb6f3b55bda938d3921f
“Ollas2204” <fitrisakinah17@gmail.com>
2018-07-27 12:34:20 +0700

    edit layout

--- src/views/Listroom.vue
+++ src/views/Listroom.vue
@@ -2,60 +2,54 @@
 <div class="listroom">
   <div class="row">
     <div class="col s12 m6 l3">
-      <div class="card">
+      <div class="card teal">
         <div class="card-image">
           <img src="../assets/room1.jpg">
           <span class="card-title">California Room</span>
+
           <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
         </div>
         <div class="card-content">
-          <p>From the casual player to the seasoned pro, this action-packed MABS room is perfect for everyone. Featuring 2 tables and weekly tournaments, The Saloon provides
-the best in live action MABS.</p>
+          <p>From the casual player to the seasoned pro, this action-packed MABS room is perfect for everyone. Featuring 2 tables and weekly tournaments, The Saloon provides the best in live action MABS.</p>
         </div>
       </div>
     </div>
-  </div>
-  <div class="row">
+
     <div class="col s12 m6 l3">
-      <div class="card">
+      <div class="card teal">
         <div class="card-image">
           <img src="../assets/room2.jpg">
           <span class="card-title">Texas Room</span>
           <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
         </div>
         <div class="card-content">
-          <p>From the casual player to the seasoned pro, this action-packed MABS room is perfect for everyone. Featuring 2 tables and weekly tournaments, The Saloon provides
-the best in live action MABS.</p>
+          <p>From the casual player to the seasoned pro, this action-packed MABS room is perfect for everyone. Featuring 2 tables and weekly tournaments, The Saloon provides the best in live action MABS.</p>
         </div>
       </div>
     </div>
-  </div>
-  <div class="row">
+
     <div class="col s12 m6 l3">
-      <div class="card">
+      <div class="card teal">
         <div class="card-image">
           <img src="../assets/room3.jpg">
           <span class="card-title">Beerhall Room</span>
           <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
         </div>
         <div class="card-content">
-          <p>From the casual player to the seasoned pro, this action-packed MABS room is perfect for everyone. Featuring 2 tables and weekly tournaments, The Saloon provides
-the best in live action MABS.</p>
+          <p>From the casual player to the seasoned pro, this action-packed MABS room is perfect for everyone. Featuring 2 tables and weekly tournaments, The Saloon provides the best in live action MABS.</p>
         </div>
       </div>
     </div>
-  </div>
-  <div class="row">
+
     <div class="col s12 m6 l3">
-      <div class="card">
+      <div class="card teal">
         <div class="card-image">
           <img src="../assets/room4.jpg">
           <span class="card-title">Panama Room</span>
           <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
         </div>
         <div class="card-content">
-          <p>From the casual player to the seasoned pro, this action-packed MABS room is perfect for everyone. Featuring 2 tables and weekly tournaments, The Saloon provides
-the best in live action MABS.</p>
+          <p>From the casual player to the seasoned pro, this action-packed MABS room is perfect for everyone. Featuring 2 tables and weekly tournaments, The Saloon provides the best in live action MABS.</p>
         </div>
       </div>
     </div>
@@ -64,9 +58,7 @@ the best in live action MABS.</p>
 </template>
 
 <script>
-export default {
-}
-
+export default {}
 </script>
 
 <style lang="css">
