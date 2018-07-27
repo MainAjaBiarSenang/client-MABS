f3fe981811b1f11e86e9ab3d54a76d1662589c31
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2018-07-27 08:41:21 +0700

    Add Layout Question Components

---
+++ src/components/QuestionLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="questionLayout">
+    <div class="container">
+      <div class="col s12 m7">
+        <h2 class="header">Question No. 1</h2>
+        <div class="card horizontal">
+          <div class="card-stacked">
+            <div class="card-content amber lighten-4">
+              <p>Question 1</p>
+            </div>
+            <div class="card-action z-depth-3">
+              
+              <div class="row">
+                <p class="blue darken-1 waves-effect waves-light btn-large col s4 offset-s1">Jawaban A</p>
+                <p class="blue darken-1 waves-effect waves-light btn-large col s4 offset-s2">Jawaban C</p>
+              </div>
+              <div class="row">
+                <div class="blue darken-1 waves-effect waves-light btn-large col s4 offset-s1">Jawaban B</div>
+                <div class="blue darken-1 waves-effect waves-light btn-large col s4 offset-s2">Jawaban D</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionLayout',
+  props: {
+    msg: String
+  }
+}
+</script>
+
